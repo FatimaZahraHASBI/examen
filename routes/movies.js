0a70e8f19552ba418a8ae01b9a2d1516564482f5
Fatima Zahra Hasbi <hasbi.fatimazahra04@gmail.com>
2022-01-21 21:49:04 +0100

    ACCES AUX DONNEES

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,23 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-var moviesJSON = require("../movies_reduit.json");
-
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send(moviesJSON);
+router.get("/", async function (req, res, next) {
+  const take = parseInt(req.query.take) || 10;
+  const skip = parseInt(req.query.skip) || 0;
+  const movies = await prisma.movies.findMany({
+    skip: skip,
+    take: take,
+  });
+  res.send({
+    data: movies,
+    pagination: {
+      count: movies.length, // Total des enregistrements
+      take: take, // Nombre d'éléments sélectionnés
+      skip: skip, // Décalage à partir duquel on prend les  données
+    },
+  });
 });
 
 module.exports = router;
